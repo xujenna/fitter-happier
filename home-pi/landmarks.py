bf9eb60721263f53b3bf143c900e3f306a8b99c2
Jenna Xu <jxu2@jenna.itp.tsoa.nyu.edu>
2019-03-26 01:38:24 -0400

    new bluetooth address

---
+++ home-pi/landmarks.py
@@ -0,0 +1,13 @@
+import json
+import random
+
+with open('../selfcare-scripts/NYC_landmarks_wiki.json', 'r') as infile:
+    data = json.load(infile)
+    keys = list(data['query']['pages'].keys())
+
+randomIndex = random.randint(0, len(keys))
+randomKey = keys[randomIndex]
+randomPage = data['query']['pages'][randomKey]
+randomPlace = randomPage['title']
+randomPlaceFormatted = randomPlace.replace(" ", "_")
+placePage = "https://en.wikipedia.org/wiki/" + randomPlaceFormatted
--- modules/playlist.js
+++ modules/playlist.js
@@ -26,7 +26,7 @@ function addToPlayQueue(file, callback) {
 
 function playFile(file, playingFromQueue) {
     // play file with callback
-    player.play(file, { aplay: ['-D', 'bluealsa:HCI=hci0,DEV=00:00:00:00:6A:59,PROFILE=a2dp'], timeout: playingFromQueue ? PLAYLIST_DELAY : 0 }, onPlayDone);
+    player.play(file, { aplay: ['-D', 'bluealsa:HCI=hci0,DEV=00:00:00:00:88:C8,PROFILE=a2dp'], timeout: playingFromQueue ? PLAYLIST_DELAY : 0 }, onPlayDone);
     // player.play(file, { timeout: playingFromQueue ? PLAYLIST_DELAY : 0 }, onPlayDone);
 
     // remove first file from queue (just played)
