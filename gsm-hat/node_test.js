ea2bb4806c791b3e1d3cf9bf0b1aa63f421da73c
Jenna Xu <jxu2@jenna.itp.tsoa.nyu.edu>
2019-03-29 20:55:50 -0400

    node serial port

--- gsm-hat/node_test.js
+++ gsm-hat/node_test.js
@@ -6,6 +6,7 @@ serialPort.on("open", function () {
     // serialPort.write("AT^SYSCFG=13,1,3FFFFFFF,2,4");
     serialPort.write("AT");
     serialPort.write('\r');
+    delay(200);
     serialPort.on('data', function(data) {
         console.log("Received data: " + data);
     });
@@ -15,14 +16,17 @@ serialPort.on("open", function () {
 function gsm_message_sending(serial, message, phone_no) {
     serial.write("AT+CMGF=1");
     serial.write('\r');
+    delay(200);
     serial.write("AT+CSCA=\"");
     serial.write("+12063130004")
     serial.write('"')
     serial.write('\r');
+    delay(200);
     serial.write("AT+CMGS=\"");
     serial.write(phone_no);
     serial.write('"')
     serial.write('\r');
+    delay(200);
     serial.write(message); 
     serial.write('\r');
     // serial.write(Buffer.from([0x1A]));
