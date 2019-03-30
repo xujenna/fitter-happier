602e89dcd266695d9b9c4e17f6edb290714fcf86
Jenna Xu <jxu2@jenna.itp.tsoa.nyu.edu>
2019-03-29 21:06:28 -0400

    node serial port

--- gsm-hat/node_test.js
+++ gsm-hat/node_test.js
@@ -1,11 +1,13 @@
 const SerialPort = require("serialport")
 const serialPort = new SerialPort("/dev/ttyS0");
 
+
+let W_buff = ["AT\r\n", "AT+CMGF=1\r\n", "AT+CSCA=\"+12063130004\"\r\n", "AT+CMGS=\"16307308188\"\r\n","helloworld"]
+
 serialPort.on("open", function () {
     console.log('Serial communication open');
     // serialPort.write("AT^SYSCFG=13,1,3FFFFFFF,2,4");
-    serialPort.write("AT");
-    serialPort.write('\r');
+    serialPort.write(W_buff[0]);
     serialPort.on('data', function(data) {
         console.log("Received data: " + data);
     });
@@ -13,21 +15,13 @@ serialPort.on("open", function () {
 });
 
 function gsm_message_sending(serial, message, phone_no) {
-    serial.write("AT+CMGF=1");
-    serial.write('\r');
-    serial.write("AT+CSCA=\"");
-    serial.write("+12063130004")
-    serial.write('"')
-    serial.write('\r');
-    serial.write("AT+CMGS=\"");
-    serial.write(phone_no);
-    serial.write('"')
-    serial.write('\r');
-    serial.write(message); 
-    serial.write('\r');
-    serial.write(Buffer.from([0x1A]));
-
-    // serial.write('^z');
+    for(let i = 1; i < W_buff.length; i++){
+        setTimeout(function(){
+            serialPort.write(W_buff[i]);
+        }, 1000)
+    }
+    serialPort.write("\x1a\r\n")
+    serialPort.write(Buffer.from([0x1A]));
 }
 
 
