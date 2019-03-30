8f7a5be6c397c21e951c18868866c6282a21642c
Jenna Xu <jxu2@jenna.itp.tsoa.nyu.edu>
2019-03-30 00:16:27 -0400

    why

--- gsm-hat/node_test.js
+++ gsm-hat/node_test.js
@@ -1,35 +1,33 @@
 const SerialPort = require("serialport")
 const serialPort = new SerialPort("/dev/ttyS0");
 
-let W_buff = ["AT+CMGF=1\r\n", "AT+CSCA=\"+12063130004\"\r\n", "AT+CMGS=\"16307308188\"\r\n","hey girl"]
+let W_buff = ["AT\r\n", "AT+CMGF=1\r\n", "AT+CSCA=\"+12063130004\"\r\n", "AT+CMGS=\"16307308188\"\r\n","hey girl"]
 
 serialPort.on("open", function () {
     console.log('Serial communication open');
-    serialPort.write("AT\r\n");
-    let count = 0;
+
     serialPort.on('data', function(data) {
         console.log("Received data: " + data);
-        count += 1;
-        console.log(count)
-        if(count == W_buff.length){
+    });
+
+    for(let i = 0; i <= W_buff.length; i++){
+        setTimeout(function(){
+            gsm_message_sending(W_buff[i]);
+        }, 5000)
+
+        if(i == W_buff.length - 1){
+            serialPort.write(W_buff[i]);
+            serialPort.write("\x1a\r\n");
+            serialPort.close();
+
+        }
+        else if(i == W_buff.length){
             serialPort.close();
         }
-        else if(count == W_buff.length - 1){
-            setTimeout(function(){
-                serialPort.write(W_buff[count]);
-                serialPort.write("\x1a\r\n");
-                serialPort.close();
-            }, 1000)
-        }
-        else if(count < W_buff.length - 1){
-            setTimeout(function(){
-                gsm_message_sending(count);
-            }, 1000)
-        }
-    });
+    }
 });
 
-function gsm_message_sending(count) {
-    serialPort.write(W_buff[count]);
-    console.log(W_buff[count])
+function gsm_message_sending(message) {
+    serialPort.write(message);
+    console.log(message)
 }
