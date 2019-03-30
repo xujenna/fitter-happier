8335bab083714d764d396ef9bc4450ce5155fbcb
Jenna Xu <jxu2@jenna.itp.tsoa.nyu.edu>
2019-03-30 00:43:26 -0400

    why

--- gsm-hat/node_test.js
+++ gsm-hat/node_test.js
@@ -2,7 +2,6 @@ const SerialPort = require("serialport")
 const serialPort = new SerialPort("/dev/ttyS0");
 
 let W_buff = ["AT\r\n", "AT+CMGF=1\r\n", "AT+CSCA=\"+12063130004\"\r\n", "AT+CMGS=\"16307308188\"\r\n","hey girl"]
-let count = 0;
 
 serialPort.on("open", function () {
     serialPort.flush()
@@ -10,15 +9,9 @@ serialPort.on("open", function () {
 
     serialPort.on('data', function(data) {
         console.log("Received data: " + data);
-        console.log(count)
-        if(data == W_buff[W_buff.length-1] && count == W_buff.length-1){
-            serialPort.flush()
-            serialPort.close()
-        }
     });
 
     for(let i = 0; i <= W_buff.length; i++){
-        count = i;
         setTimeout(function(){
             gsm_message_sending(W_buff[i]);
         }, 5000)
@@ -34,5 +27,9 @@ function gsm_message_sending(message) {
     if(message !== undefined){
         serialPort.write(message);
         console.log(message)
+    }
+    else{
+        serialPort.flush()
+        serialPort.close()
     }
 }
