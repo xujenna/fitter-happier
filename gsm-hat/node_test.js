37655b8b99c0e8003d27613329cecc8ea3a12a39
Jenna Xu <jxu2@jenna.itp.tsoa.nyu.edu>
2019-03-29 20:59:32 -0400

    node serial port

--- gsm-hat/node_test.js
+++ gsm-hat/node_test.js
@@ -1,12 +1,11 @@
-var SerialPort = require("serialport")
-var serialPort = new SerialPort("/dev/ttyS0");
+const SerialPort = require("serialport")
+const serialPort = new SerialPort("/dev/ttyS0");
 
 serialPort.on("open", function () {
     console.log('Serial communication open');
     // serialPort.write("AT^SYSCFG=13,1,3FFFFFFF,2,4");
     serialPort.write("AT");
     serialPort.write('\r');
-    delay(200);
     serialPort.on('data', function(data) {
         console.log("Received data: " + data);
     });
@@ -16,21 +15,51 @@ serialPort.on("open", function () {
 function gsm_message_sending(serial, message, phone_no) {
     serial.write("AT+CMGF=1");
     serial.write('\r');
-    delay(200);
     serial.write("AT+CSCA=\"");
     serial.write("+12063130004")
     serial.write('"')
     serial.write('\r');
-    delay(200);
     serial.write("AT+CMGS=\"");
     serial.write(phone_no);
     serial.write('"')
     serial.write('\r');
-    delay(200);
     serial.write(message); 
     serial.write('\r');
-    // serial.write(Buffer.from([0x1A]));
-    // serial.write(Buffer.from([0x1A]));
+    serial.write(Buffer.from([0x1A]));
 
     // serial.write('^z');
 }
+
+
+
+
+
+// serialPort.on("open", function () {
+//     console.log('Serial communication open');
+//     // serialPort.write("AT^SYSCFG=13,1,3FFFFFFF,2,4");
+//     serialPort.write("AT");
+//     serialPort.write('\r');
+//     serialPort.on('data', function(data) {
+//         console.log("Received data: " + data);
+//     });
+//     gsm_message_sending(serialPort, "hey girl", "16307308188");
+// });
+
+// function gsm_message_sending(serial, message, phone_no) {
+//     serial.write("AT+CMGF=1");
+//     serial.write('\r');
+//     serial.write("AT+CSCA=\"");
+//     serial.write("+12063130004")
+//     serial.write('"')
+//     serial.write('\r');
+//     serial.write("AT+CMGS=\"");
+//     serial.write(phone_no);
+//     serial.write('"')
+//     serial.write('\r');
+//     serial.write(message); 
+//     serial.write('\r');
+//     // serial.write(Buffer.from([0x1A]));
+//     // serial.write(Buffer.from([0x1A]));
+
+//     // serial.write('^z');
+// }
