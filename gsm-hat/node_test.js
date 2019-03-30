a7d07b43e53ae5f36182a72efc21488f4d625a4b
Jenna Xu <jxu2@jenna.itp.tsoa.nyu.edu>
2019-03-30 17:04:53 -0400

    more gsm testing

--- gsm-hat/node_test.js
+++ gsm-hat/node_test.js
@@ -2,22 +2,11 @@ const SerialPort = require("serialport")
 const serialPort = new SerialPort("/dev/ttyS0");
 const Readline = require('@serialport/parser-readline')
 
-let W_buff = ["AT\r\n", "AT+CMGF=1\r\n", "AT+CSCA=\"+12063130004\"\r\n", "AT+CMGS=\"16307308188\"\r\n","hey girl"]
+let W_buff = ["AT\r\n", "AT+CMGF=1\r\n", "AT+CSCA=\"+12063130004\"\r\n", "AT+CMGS=\"16307308188\"\r\n","hey girl\r\n"]
 
 let count = 0;
 
 const parser = serialPort.pipe(new Readline({ delimiter: '\r\n' }))
-parser.on('data', function(data){
-    console.log(data);
-    if(data == "OK"){
-        count += 1;
-    }
-    if(count == W_buff.length - 1){
-        count = 0;
-        serialPort.flush();
-        serialPort.close();
-    }
-})
 
 
 serialPort.on("open", function () {
@@ -28,7 +17,17 @@ serialPort.on("open", function () {
         console.log("Received data: " + data);
     });
 
-
+    parser.on('data', function(data){
+        console.log(data);
+        if(data == "OK"){
+            count += 1;
+        }
+        if(count >= W_buff.length){
+            count = 0;
+            serialPort.flush();
+            serialPort.close();
+        }
+    })
 
     for(let i = 0; i <= W_buff.length -1; i++){
         setTimeout(function(){
