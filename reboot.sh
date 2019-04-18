d6735fb4a7bbaa3f954cebfa544b8c44f4174e32
Jenna Xu <jxu2@jenna.itp.tsoa.nyu.edu>
2019-04-18 01:56:37 -0400

    lolol

--- reboot.sh
+++ reboot.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 su - pi <<!
-sleep 15
+sleep 20
 ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && echo ‘ok’ || (python /home/pi/fitter-happier/gsm-hat/GSM_PWRKEY.py)
 sudo bluetoothctl 
 connect 53:B7:C7:01:02:F2
