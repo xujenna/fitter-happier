b36055aaa052749109404e84ba46f0cec56e4498
Jenna Xu <jxu2@jenna.itp.tsoa.nyu.edu>
2019-04-24 17:26:34 -0400

    cron jobs

--- reboot.sh
+++ reboot.sh
@@ -4,7 +4,7 @@ su - pi <<!
 sudo bluetoothctl 
 connect 53:B7:C7:01:02:F2
 quit
-ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && echo ‘ok’ || (python /home/pi/fitter-happier/gsm-hat/GSM_PWRKEY.py sleep 20)
+ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && echo ‘ok’ || (python /home/pi/fitter-happier/gsm-hat/GSM_PWRKEY.py && sleep 10 && sudo pon fona && sleep 5)
 cd /home/pi/fitter-happier/
 npm run cred node index_rituals.js
 ip addr | grep ppp && (python /home/pi/fitter-happier/gsm-hat/GSM_PWRKEY.py) || echo ‘done’
