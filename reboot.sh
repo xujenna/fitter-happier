0b3fcf5c65d6fd374a214e481c406b84bc4afc9c
Jenna Xu <jxu2@jenna.itp.tsoa.nyu.edu>
2019-04-24 16:54:31 -0400

    oops

--- reboot.sh
+++ reboot.sh
@@ -4,8 +4,7 @@ su - pi <<!
 sudo bluetoothctl 
 connect 53:B7:C7:01:02:F2
 quit
-ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && echo ‘ok’ || (python /home/pi/fitter-happier/gsm-hat/GSM_PWRKEY.py sleep 20
-)
+ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && echo ‘ok’ || (python /home/pi/fitter-happier/gsm-hat/GSM_PWRKEY.py sleep 20)
 cd /home/pi/fitter-happier/
 npm run cred node index_rituals.js
 ip addr | grep ppp && (python /home/pi/fitter-happier/gsm-hat/GSM_PWRKEY.py) || echo ‘done’
