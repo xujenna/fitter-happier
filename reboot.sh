c82a13ef540351b6bac021ea830d5c3dfe067426
Jenna Xu <jxu2@jenna.itp.tsoa.nyu.edu>
2019-04-25 01:50:10 -0400

    cron jobs

--- reboot.sh
+++ reboot.sh
@@ -7,6 +7,6 @@ quit
 ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && echo ‘ok’ || (python /home/pi/fitter-happier/gsm-hat/GSM_PWRKEY.py && sleep 10 && sudo pon fona && sleep 5)
 cd /home/pi/fitter-happier/
 npm run cred node index_rituals.js &
-sleep 35
+sleep 100
 ip addr | grep ppp && (python /home/pi/fitter-happier/gsm-hat/GSM_PWRKEY.py) || echo ‘done’
 !
